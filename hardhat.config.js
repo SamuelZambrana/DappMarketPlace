7631eb98b5f67ac1f0efd763043abc72e1e9fd2c
Perdi Gonzalez <perdiweb3@gmail.com>
2024-02-11 13:12:25 +0100

    deploy testnet

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,20 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config(
+  {path:".env"}
+);
+
+const ALCHEMY_HTTP_KEY = process.env.ALCHEMY_HTTP_KEY
+const DEPLOYER_PRIVATE_KEY = process.env.DEPLOYER_PRIVATE_KEY
+
+// const { ALCHEMY_HTTP_KEY, DEPLOYER_PRIVATE_KEY } = process.env;
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.19",
+  networks:{
+    mumbai:{
+      url:ALCHEMY_HTTP_KEY,
+      accounts: [DEPLOYER_PRIVATE_KEY]
+    }
+  }
 };
