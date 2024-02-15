757eed5d9bec9e1cfe473d4eebe9e4f4efeb66ca
SamuelZambrana <samu-1994@hotmail.es>
2024-02-15 13:09:53 +0100

    Deploy MarketPlace

--- scripts/MarketPlace.deploy.js
+++ scripts/MarketPlace.deploy.js
@@ -1,12 +1,15 @@
 const hre = require("hardhat");
 
+const ERC20DeployScript = require("./ERC20.deploy")
+const ERC721DeployScript = require("./ERC721.deploy")
+
 let deployedMarketPlaceContract
 let contractAddress
 
 async function deploy(){
     console.log("MarketPlace deployment has just started...")
     const marketPlaceContract = await ethers.getContractFactory("MyMarketPlace")
-    deployedMarketPlaceContract = await marketPlaceContract.deploy("","")
+    deployedMarketPlaceContract = await marketPlaceContract.deploy(ERC20DeployScript.contractAddress, ERC721DeployScript.contractAddress)
     await deployedMarketPlaceContract.waitForDeployment()
     contractAddress = deployedMarketPlaceContract.target
     console.log("...MarketPlace constract has been deployed to: " + contractAddress)
