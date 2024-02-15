1f2de82de4857e6fd3f25d2af70cbe52ad4968e7
SamuelZambrana <samu-1994@hotmail.es>
2024-02-15 13:14:20 +0100

    Deploy MarketPlace

--- scripts/deployment_manager.js
+++ scripts/deployment_manager.js
@@ -1,12 +1,12 @@
 
 const ERC20DeployScript = require("./ERC20.deploy")
 const ERC721DeployScript = require("./ERC721.deploy")
-//const MarketPlaceDeployScript = require("./MarketPlace.deploy")
+const MarketPlaceDeployScript = require("./MarketPlace.deploy")
 
 const main = async () => {
     await ERC20DeployScript.deploy()
     await ERC721DeployScript.deploy()
-    //await MarketPlaceDeployScript.deploy()
+    await MarketPlaceDeployScript.deploy()
 }
 
 main()
