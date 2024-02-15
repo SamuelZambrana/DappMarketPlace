4ba48d4bb28bd0113b18ca6d15e7e9d95b3145a5
SamuelZambrana <samu-1994@hotmail.es>
2024-02-15 18:24:31 +0100

    Deploy MarketPlace

--- scripts/deployment_manager.js
+++ scripts/deployment_manager.js
@@ -5,8 +5,10 @@ const MarketPlaceDeployScript = require("./MarketPlace.deploy")
 
 const main = async () => {
     await ERC20DeployScript.deploy()
+    //Funcion Hererada que tiene en comun todos los deployer heredados  que devuelve el contractAdress
     let ERC20ContractAddress = await ERC20DeployScript.getContractAddress()
     await ERC721DeployScript.deploy()
+    //Funcion Hererada que tiene en comun todos los deployer heredados  que devuelve el contractAdress
     let ERC721ContractAddress = await ERC721DeployScript.getContractAddress()
     await MarketPlaceDeployScript.deploy(ERC20ContractAddress, ERC721ContractAddress)
 }
--- test/MarketPlace.test.js
+++ test/MarketPlace.test.js
@@ -2,16 +2,30 @@ const { expect } = require("chai");
 
 describe("MarketPlace Test Suite", function(){
 
-    let deployedMarketPlaceContract
+    let deployedMarketPlaceContract, deployedERC20Contract, deployedERC721Contract
 
     let signer, otherAccount, tokenContractAddress, ownerAddress //Signers
     let tokenId, saleId, price, buyer // Sales
 
-    it("Deploy Contract", async function(){
+    it("Deploy Contract ERC20", async function(){
+        const ERC20Contract = await ethers.getContractFactory("MyCoin")
+        deployedERC20Contract = await ERC20Contract.deploy(5000,2)
+        await deployedERC20Contract.waitForDeployment()
+        //console.log(deployedERC20Contract.target)
+    })
+
+    it("Deploy Contract ERC721", async function(){
+        const ERC71Contract = await ethers.getContractFactory("MyNFTCollection")
+        deployedERC721Contract = await ERC71Contract.deploy("NFTCollection","CoNFT")
+        await deployedERC721Contract.waitForDeployment()
+        //console.log(deployedERC721Contract.target)
+    })
+
+    it("Deploy Contract MarketPlace", async function(){
         const marketPlaceContract = await ethers.getContractFactory("MyMarketPlace")
-        deployedMarketPlaceContract = await marketPlaceContract.deploy("","")
+        deployedMarketPlaceContract = await marketPlaceContract.deploy(deployedERC20Contract.target,deployedERC721Contract.target)
         await deployedMarketPlaceContract.waitForDeployment()
-        console.log(deployedMarketPlaceContract.target)
+        //console.log(deployedMarketPlaceContract.target)
         const amount = 100
         //Llama al método "approve" en el contrato desplegado para autorizar la direccion
         //tokenContractAddress que pueda hacer transferencias.
