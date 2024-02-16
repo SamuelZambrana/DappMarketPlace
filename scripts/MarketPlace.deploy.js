43dcb96cffdc5faaee1f6805924eaa4f6d1875de
SamuelZambrana <samu-1994@hotmail.es>
2024-02-16 03:40:56 +0100

    Deploy MarketPlace

--- scripts/MarketPlace.deploy.js
+++ scripts/MarketPlace.deploy.js
@@ -3,15 +3,17 @@ const hre = require("hardhat");
 const ERC20DeployScript = require("./ERC20.deploy")
 const ERC721DeployScript = require("./ERC721.deploy")
 
-let deployedMarketPlaceContract, ERC20DeployScript, ERC721DeployScript
+let deployedMarketPlaceContract
 let contractAddress
 
 async function deploy(){
     console.log("MarketPlace deployment has just started...")
     const marketPlaceContract = await ethers.getContractFactory("MyMarketPlace")
-    await ERC20DeployScript.deployedERC20Contract
-    await ERC721DeployScript.deployedERC721Contract
-    deployedMarketPlaceContract = await marketPlaceContract.deploy(ERC20DeployScript.contractAddress, ERC721DeployScript.contractAddress)
+    // Despliega los contratos ERC20 y ERC721
+    const erc20Contract = await ERC20DeployScript.deployedERC20Contract();
+    const erc721Contract = await ERC721DeployScript.deployedERC721Contract();
+    // Pasa las direcciones de los contratos como parámetros al contrato de MarketPlace
+    deployedMarketPlaceContract = await marketPlaceContract.deploy(erc20Contract.target, erc721Contract.target)
     await deployedMarketPlaceContract.waitForDeployment()
     contractAddress = deployedMarketPlaceContract.target
     console.log("...MarketPlace constract has been deployed to: " + contractAddress)
