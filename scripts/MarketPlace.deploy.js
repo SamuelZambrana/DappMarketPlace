dc123e10cb48e3ea056f673279ba047fb3d5b72a
SamuelZambrana <samu-1994@hotmail.es>
2024-02-16 03:32:32 +0100

    Deploy MarketPlace

--- scripts/MarketPlace.deploy.js
+++ scripts/MarketPlace.deploy.js
@@ -9,8 +9,9 @@ let contractAddress
 async function deploy(){
     console.log("MarketPlace deployment has just started...")
     const marketPlaceContract = await ethers.getContractFactory("MyMarketPlace")
-    ERC20DeployScript = await deployedERC20Contract.deploy()
-    ERC721DeployScript = await deployedERC721Contract.deploy()
+    await ERC20DeployScript.deployedERC20Contract
+    await ERC721DeployScript.deployedERC721Contract
+    ERC721DeployScript = await deployedERC721Contract
     deployedMarketPlaceContract = await marketPlaceContract.deploy(ERC20DeployScript.contractAddress, ERC721DeployScript.contractAddress)
     await deployedMarketPlaceContract.waitForDeployment()
     contractAddress = deployedMarketPlaceContract.target
